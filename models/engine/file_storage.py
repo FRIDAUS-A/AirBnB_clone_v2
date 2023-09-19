fa19784d1e0995ed06906d9a45fc163113d97753
FRIDAUS <fridausokoya@gmail.com>
2023-09-19 13:08:42 +0100

    Delete object method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,16 +16,24 @@ class FileStorage():
     __file_path = "./file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns the dictionary __objects"""
-        return FileStorage.__objects
-    
+        total = FileStorage.__objects
+        if cls:
+            new_dict = {}
+            for key, value in total.items():
+                if self.classes()[value.to_dict()["__class__"]] == cls:
+                    new_dict[key] = value
+            return new_dict
+        else:
+            return (FileStorage.__objects)
+
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         key = f"{obj.__class__.__name__}.{obj.id}"
         FileStorage.__objects[key] = obj
         return
-    
+
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         new_dict = {}
@@ -48,25 +56,39 @@ class FileStorage():
         from models.review import Review
 
         classes = {"BaseModel": BaseModel,
-                    "User": User,
-                    "Place": Place,
-                    "State": State,
-                    "City": City,
-                    "Amenity": Amenity,
-                    "Review": Review}
+                   "User": User,
+                   "Place": Place,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Review": Review}
         return classes
-
 
     def reload(self):
         """
-        deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; 
-        otherwise, do nothing. If the file doesn’t exist, no exception should be raised)
+        deserializes the JSON file to __objects (only if the JSON
+        file (__file_path) exists ;
+        otherwise, do nothing. If the file doesn’t exist,
+        no exception should be raised)
         """
         try:
             with open(FileStorage.__file_path, "r") as file:
                 content = file.read()
                 content = json.loads(content)
                 for key, value in content.items():
-                    FileStorage.__objects[key] = self.classes()[value["__class__"]](**value)
+                    FileStorage.__objects[key] = self.classes()
+                    [value["__class__"]](**value)
         except FileNotFoundError:
             return
+
+    def delete(self, obj=None):
+        """delete obj from __objects if its
+        inside and do nothing if obj is None"""
+        if obj:
+            content = self.all()
+            for key, value in content.items():
+                if value.to_dict()['id'] == obj.id:
+                    content.pop(key)
+                    break
+            for key, value in content.items():
+                self.new(value)
