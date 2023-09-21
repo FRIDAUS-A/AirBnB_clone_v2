5500ae4d2552900efc3da0013b9c21b8f45329c9
FRIDAUS <fridausokoya@gmail.com>
2023-09-21 16:32:17 +0100

    State class

--- models/state.py
+++ models/state.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
 """Inherits from the BaseModel class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-class State(BaseModel):
+
+class State(BaseModel, Base):
     """State"""
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state")
+
+    @property
+    def cities(self):
+        content_state = storage.all(self)
+        content_city = storage.all(City)
+        common = []
+        for value_state in content_state.values():
+            for value_city in content_city.values():
+                if value_state.to_dict()["id"] == value_city.to_dict["id"]:
+                    common.append(value_state)
+        return (common)
