a7f3b2a94004bef2952f3466facc510606e0b189
FRIDAUS <fridausokoya@gmail.com>
2023-09-21 16:32:38 +0100

    City

--- models/city.py
+++ models/city.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """Inheits from the BaseModel class"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, Integer, ForeignKey
 
-class City(BaseModel):
+
+class City(BaseModel, Base):
     """city class"""
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128), nullable=False)
