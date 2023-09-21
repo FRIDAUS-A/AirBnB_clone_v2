58049472d715445bc4f1f8d85090ce328d98cfa4
FRIDAUS <fridausokoya@gmail.com>
2023-09-21 16:34:02 +0100

    __init__

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+from os import environ
 
-from models.engine.file_storage import FileStorage
-
-storage = FileStorage()
+if 'HBNB_TYPE_STORAGE' in environ and environ['HBNB_TYPE_STORAGE'] == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 storage.reload()
