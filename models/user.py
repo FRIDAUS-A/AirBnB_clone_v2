130e92e9850d7e2078cf17c386d5db8b7cf9fcb5
FRIDAUS <fridausokoya@gmail.com>
2023-09-21 17:00:03 +0100

    User

--- models/user.py
+++ models/user.py
@@ -3,10 +3,11 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 
+
 class User(BaseModel, Base):
-        """The constructor for the user class"""
-        __tablename__ = "users"
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
+    """The constructor for the user class"""
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
